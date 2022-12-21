b43bf344ab62c24a1314649d1981f5bb5a0b0e71
josei73 <108838120+josei73@users.noreply.github.com>
2022-12-21 22:00:38 +0100

    Update OrderRestController.java

--- src/main/java/de/hsbremen/mkss/restservice/controller/OrderRestController.java
+++ src/main/java/de/hsbremen/mkss/restservice/controller/OrderRestController.java
@@ -32,26 +32,28 @@ public class OrderRestController {
     @Autowired
     private OrderService orderService;
 
-
+    // Takes an id parameter from the URL path and uses it to retrieve an Order object
     @GetMapping(value = "/orders/{id}")
     public Order getOrder(@PathVariable Integer id) throws OrderNotFoundException {
 
         return orderService.get(id);
 
     }
-
+    //  Retrieves a list of all Order objects from the orderService.
     @GetMapping("orders")
     public List<Order> getAllOrders() {
-
         return orderService.findAll();
     }
 
+
+
+    // Takes an id parameter from the URL path and uses it to retrieve a set of LineItem objects for the corresponding Order
     @GetMapping(value = "/orders/{id}/items")
     public Set<LineItem> getAllOrderItems(@PathVariable Integer id) throws OrderNotFoundException {
         return orderService.getItems(id);
 
     }
-
+    // Takes an Order object as a request body, which is validated using the @Valid and @Nonnull annotations.
     @PostMapping(value = "orders")
     @ResponseStatus(value = HttpStatus.CREATED)
     public ChangeResultView addOrder(@Nonnull @Valid @RequestBody Order order, BindingResult bindingResult) {
@@ -63,6 +65,7 @@ public class OrderRestController {
 
     }
 
+    // This method is similar to the addOrder method, but it takes an id parameter from the URL path and a LineItem object as a request body. It uses these parameters to add the item to the specified order.
     @PostMapping(value = "/orders/{id}/items")
     @ResponseStatus(value = HttpStatus.CREATED)
     public ChangeResultView addItemToOrder(@PathVariable Integer id, @Valid @Nonnull @RequestBody LineItem lineItem, BindingResult bindingResult) throws OrderNotFoundException, OrderStatusException {
@@ -70,23 +73,23 @@ public class OrderRestController {
         if (bindingResult.hasErrors()) {
             return ChangeResultView.from(bindingResult);
         }
-
-
         return ChangeResultView.update(orderService.addItem(lineItem, id));
-
 
     }
 
+    //  Takes an id parameter from the URL path and uses it to mark the corresponding order as checked out
     @PutMapping(value = "/orders/{id}/checkout")
     public ChangeResultView purchaseOrder(@PathVariable Integer id) throws OrderNotFoundException, OrderStatusException {
         return ChangeResultView.update(orderService.checkout(id));
     }
 
+    // Iakes oid and lid parameters from the URL path, which represent the IDs of the order and the line item to be removed, respectively
     @DeleteMapping(value = "/orders/{oid}/items/{lid}")
     public ChangeResultView removeItemFromOrder(@PathVariable Integer oid, @PathVariable Integer lid) throws OrderNotFoundException, ItemNotFound, OrderStatusException {
         return ChangeResultView.delete(orderService.removeItem(lid, oid));
     }
 
+    // Takes an id parameter from the URL path and uses it to delete the corresponding Order
     @DeleteMapping("/orders/{id}")
     public ChangeResultView deleteOrder(@PathVariable int id) throws OrderNotFoundException {
         return ChangeResultView.delete(orderService.delete(id));
